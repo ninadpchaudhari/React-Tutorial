fe8264cbae661223f7968addd0243711c238f3b9
Ninad P.Chaudhari <nchaudhari@albany.edu>
2020-02-24 17:04:45 -0500

    Server side done

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/hello', (req,res)=>{
+  res.json({"Greeting" : "Hello " + req.body.name});
+});
+
+router.get('/add/:firstNumber/and/:secondNumber', (req,res)=>{
+  console.log(req.params.firstNumber + req.params.secondNumber);
+  let firstNo = parseInt(req.params.firstNumber),
+      secondNo = parseInt(req.params.secondNumber);
+  res.json({"Addition" : firstNo + secondNo});
+});
+
 module.exports = router;
